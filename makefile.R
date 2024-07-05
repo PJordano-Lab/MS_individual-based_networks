39338badeaad086f5d3a703c401cc0db043eb7f3
Elena Quintero <elenaquintero.qb@gmail.com>
2024-07-05 11:16:44 +0200

    MAJOR update repo - new data and analysis

--- makefile.R
+++ makefile.R
@@ -4,37 +4,78 @@ library(rmarkdown)
 
 #### DATA PREPARATION #####
 
-# Each network has been standardized dividing each interaction by the total number
-# of interactions (Grand Total Standardization). All standardized interactions matrices
-# are in ("networks/standardized").
-render("analysis/webs_standarization.Rmd")
+# Control networks by sampling effort and standardize networks
+# (both for species-based and individual-based)
 
-# To obtain main web metrics:
-render("analysis/mod_estimation.Rmd")
-render("analysis/web_metrics.Rmd")
-render("analysis/node_metrics.Rmd")
+render("analysis/nets_standarization.Rmd")
 
-# Selection of webs for analysis:
-render("analysis/web_selection.Rmd") #filter by non-representative nets and include network characteristics
+# FOR INDIVIDUAL-BASED NETWORKS:
 
-# To asses sampling completeness:
+# Bayesian modelling of interaction counts
+
+nets <- list.files(here::here("networks/nets_raw"), pattern = "int.csv")
+nets <- sort(gsub("_int.csv", "", nets))
+params <- list(beta = 0.01, iter = 10000, model = "varying_preferences")
+for (net in nets) {
+  params$net <- net
+  if (net %in% c("11_01", "15_01", "16_03", "16_04", "19_01")) {
+    params$iter = 50000
+  }
+  if (net %in% c("02_01")) {
+    params$iter = 100000
+  }
+  render(input = "analysis/Bayes_varying_pref.Rmd",
+         output_file = net,
+         output_dir = "analysis/output/Modelling/",
+         knit_root_dir = here::here(),
+         params = params)
+}
+
+# Control networks posteriors by sampling effort and standardize networks
+
+render("analysis/nets_standarization_Bayes.Rmd")
+
+
+#####
+
+# Asses sampling completeness
+
 render("analysis/sampling_completeness.Rmd")
 
-# Selection of frugivore attributes for analysis:
+# Obtain network-level metrics for species-based and individual-based nets
+
+render("analysis/net_level_metrics.Rmd")
+
+# Null models for network-level metrics for species-based and individual-based nets
+
+render("analysis/net_level_null_models.Rmd")
+
+# Obtain web metrics for individual-based network posteriors
+
+render("analysis/net_metrics_posteriors.Rmd")
+
+# Selection of frugivore attributes for analysis
+
 render("analysis/prep_frug_attributes.Rmd")
 
-# Calculate frugivore contribution to overall interactions:
+# Calculate frugivore contribution to overall interactions (Fig. 4)
+
 render("analysis/frugivore_contribution.Rmd")
 
-# Metrics selection:
+# Selection of network metrics
+
 render("analysis/selection_net_level.Rmd") # Explore net-level metrics
 render("analysis/selection_node_level.Rmd") # Explore node-level metrics
 
-# PCA analysis of NET-LEVEL and NODE-LEVEL metrics:
+# Compare individual-based and sp-based networks (Fig. 2)
+
 render("analysis/net_level_comparison.Rmd")
-render("analysis/node_level_analysis.Rmd")
-render("analysis/PCA_3d_plot.Rmd")
 
-# Analysis individual specialization:
-render("analysis/ind_specialization.Rmd")
+# Calculate Individual specialization (Fig. 3)
 
+render("analysis/WIC_TNW_calc.Rmd")
+
+# Node-level analyses for individual-based networks (Fig. 5)
+
+render("analysis/PCA_cluster_analysis.Rmd")
+render("analysis/PCA_plant_individuals.Rmd")
